951dcece77df9c6d8288d35c113b83304774d89f
Avdo Sator <avdo.sator@hotmail.com>
2023-12-31 11:15:35 +0100

    Introduce basic custom error handling middleware

--- Section 42 - Handling Errors In Express Apps/first steps/index.js
+++ Section 42 - Handling Errors In Express Apps/first steps/index.js
@@ -5,8 +5,8 @@ const morgan = require("morgan");
 app.use(morgan("dev"));
 
 // this will be executed on every single request
-app.use((req,res,next) => {
-    console.log("hello");
+app.use((req, res, next) => {
+    console.log("Every request hits this middleware!");
     next();
 });
 
@@ -15,13 +15,13 @@ const verifyPassword = (req, res, next) => {
     const {password} = req.query;
     if(password === "correctpassword") {
         next();
-    } else {
-        res.send("Your password is not correct! Try again.");
-    }
+    }  
+    //res.send("Your password is not correct! Try again.");
+    throw new Error("Password required!");
 }
 
 // this will be executed only if we hit /dogs route, but it will hit app.get("/dogs") because we call next() in it
-app.use("/dogs", (req,res,next) => {
+app.use("/dogs", (req, res, next) => {
     console.log("I have big dogs");
     next();
 });
@@ -38,9 +38,18 @@ app.get("/secret", verifyPassword, (req, res) => {
     res.send("My secret is that I have one million euros in my basement");
 });
 
+app.get("/error", (req, res) => {
+    avdo.eat();
+});
+
 app.use((req, res) => {
     res.status(404).send("NOT FOUND!");
 });
+
+app.use((err, req, res, next) => {
+    res.send("Error!!!!!!");
+    // next(err); if we want to hit express built in (default) error handler we call next like this (it doesnt make sense to do this)
+})
 
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000");
