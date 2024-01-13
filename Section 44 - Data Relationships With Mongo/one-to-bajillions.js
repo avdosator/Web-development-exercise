844cf60fb867098de53c5ae3217ccd4fe99b016a
Avdo Sator <avdo.sator@hotmail.com>
2024-01-13 12:31:33 +0100

    Different way of using populate() method

--- Section 44 - Data Relationships With Mongo/one-to-bajillions.js
+++ Section 44 - Data Relationships With Mongo/one-to-bajillions.js
@@ -29,3 +29,12 @@ const makeTweets = async () => {
     user.save();
     tweet1.save();
 }
+
+//makeTweets();
+
+const findTweet = async () => {
+    const tweet = await Tweet.findOne({}).populate("user", "username"); // a way to use populate() to just show us username field
+    console.log(tweet)
+}
+
+findTweet();
