35364f2511a92cd48930eb52e82f2979da623906
Avdo Sator <avdo.sator@hotmail.com>
2024-01-13 12:05:08 +0100

    Create file to show one to bajillions relationship and connect to database

---
+++ Section 44 - Data Relationships With Mongo/one-to-bajillions.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+mongoose.connect("mongodb://127.0.0.1:27017/relationshipDemo")
+    .then(() => {
+        console.log("Connected to database");
+    }).catch(err => {
+        console.log(err);
+    });
