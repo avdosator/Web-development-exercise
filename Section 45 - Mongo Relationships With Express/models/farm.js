19e4db5e678299ffb731e57d49771d7b7459a886
Avdo Sator <avdo.sator@hotmail.com>
2024-01-17 20:19:49 +0100

    Create Farm schema and model

---
+++ Section 45 - Mongo Relationships With Express/models/farm.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const {Schema} = mongoose;
+
+const farmSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "Farm must have a name"]
+    },    
+    city: {
+        type: String,
+        required: [true, "Please provide a city"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"]
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Product"
+        }
+    ]
+});
+
+const Farm = mongoose.model("Farm", farmSchema);
+
+module.exports = Farm;
