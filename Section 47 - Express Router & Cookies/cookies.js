ccb48cacd9343f6432332d4a05fe55b70a2482b6
Avdo Sator <avdo.sator@hotmail.com>
2024-01-24 11:32:48 +0100

    Implement route for sending cookies from server

--- Section 47 - Express Router & Cookies/cookies.js
+++ Section 47 - Express Router & Cookies/cookies.js
@@ -5,6 +5,12 @@ app.get("/greet", (req, res) => {
     res.send("HELLO THERE");
 });
 
+app.get("/setcookie", (req, res) => {
+    res.cookie("name", "avdo");
+    res.cookie("surname", "sator");
+    res.send("Cookies sent");
+});
+
 app.listen(3000, () => {
     console.log("CONNECTED");
 });
