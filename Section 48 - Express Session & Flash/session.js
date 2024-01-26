53f442cdd351c4bedbd02129c6954f281a28fb4c
Avdo Sator <avdo.sator@hotmail.com>
2024-01-26 23:51:05 +0100

    Show how we can access session data

--- Section 48 - Express Session & Flash/session.js
+++ Section 48 - Express Session & Flash/session.js
@@ -4,6 +4,15 @@ const session = require("express-session");
 
 app.use(session({ secret: "badsecret" }));
 
+app.get("/viewcount", (req, res) => {
+    if(req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send(`You viewed this page ${req.session.count} times`);
+});
+
 app.listen(3000, () => {
     console.log("Connected");
 });
