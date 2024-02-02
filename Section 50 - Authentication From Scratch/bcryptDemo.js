69469cf366f7dd7aacfde6307c963ac36fa44f9a
Avdo Sator <avdo.sator@hotmail.com>
2024-02-02 13:39:54 +0100

    Show how compare() method works

--- Section 50 - Authentication From Scratch/bcryptDemo.js
+++ Section 50 - Authentication From Scratch/bcryptDemo.js
@@ -7,4 +7,14 @@ const hashPassword = async function(password) {
     console.log(hashedPw);
 }
 
-hashPassword("ihavebigdog");
+const login = async (pw, hashedPw) => {
+    const isValid = await bcrypt.compare(pw, hashedPw);
+    if(isValid) {
+        console.log("You are logged in!!!!!!");
+    } else {
+        console.log("Incorrect password!! Try again!");
+    }
+}
+
+// hashPassword("ihavebigdog");
+login("ihavebigdog", "$2b$12$TzQQXB0dpspK1rVkWca9xeTjVwIQp5A9MyCJwj/09sMPkBliEg5Ze");
