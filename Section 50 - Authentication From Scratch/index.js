223623e16aa59da336f8d25ff926bd29fdbfc2af
Avdo Sator <avdo.sator@hotmail.com>
2024-02-03 10:13:04 +0100

    Add route for rendering login template

--- Section 50 - Authentication From Scratch/index.js
+++ Section 50 - Authentication From Scratch/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({extended: "true"}));
+app.use(express.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://127.0.0.1:27017/authDemo") 
     .then(() => {                                      
